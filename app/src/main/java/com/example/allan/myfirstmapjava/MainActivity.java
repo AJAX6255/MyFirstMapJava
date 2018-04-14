e32ec8c8f34ac0a4ec3c9d71b2719fe63d922f9c
Allan Jackson <allan.jackson6255@gmail.com>
2018-04-13 20:50:28 -0700

    Initial commit

---
+++ app/src/main/java/com/example/allan/myfirstmapjava/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.allan.myfirstmapjava;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.esri.arcgisruntime.mapping.ArcGISMap;
+import com.esri.arcgisruntime.mapping.Basemap;
+import com.esri.arcgisruntime.mapping.view.MapView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private MapView mMapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // inflate MapView from layout
+        mMapView = (MapView) findViewById(R.id.mapView);
+        // create a map with the BasemapType topographic
+        ArcGISMap map = new ArcGISMap(Basemap.Type.TOPOGRAPHIC, 32.321595, -110.895800, 16);
+        // set the map to be displayed in this view
+        mMapView.setMap(map);
+    }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+        mMapView.pause();
+    }
+
+    @Override
+    protected void onResume(){
+        super.onResume();
+        mMapView.resume();
+    }
+}
